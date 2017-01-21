c573ba7b5bcabd839571f7dc3d54ae775f9ffdfb
Per Rovegård <per@rovegard.se>
2017-01-21 13:44:37 +0100

    Adding more real-marshalling test cases

--- src/test/scala/com/programmaticallyspeaking/ncd/nashorn/RealMarshallerTest.scala
+++ src/test/scala/com/programmaticallyspeaking/ncd/nashorn/RealMarshallerTest.scala
@@ -1,20 +1,36 @@
 package com.programmaticallyspeaking.ncd.nashorn
 
-import com.programmaticallyspeaking.ncd.host.SimpleValue
+import com.programmaticallyspeaking.ncd.host.types.Undefined
+import com.programmaticallyspeaking.ncd.host.{EmptyNode, SimpleValue}
 
 class RealMarshallerTest extends RealMarshallerTestFixture {
 
   val marshalledValues = Table(
     ("desc", "expression", "expected"),
     ("string", "'hello world'", SimpleValue("hello world")),
-    ("integer value", "42", SimpleValue(42))
+    ("integer value", "42", SimpleValue(42)),
+    ("floating-point value", "42.5", SimpleValue(42.5d)),
+    ("boolean value", "true", SimpleValue(true)),
+    ("null", "null", EmptyNode),
+    ("undefined", "undefined", SimpleValue(Undefined)),
+    ("NaN", "NaN", SimpleValue(Double.NaN))
   )
 
   "Marshalling works for" - {
     forAll(marshalledValues) { (desc, expr, expected) =>
       desc in {
         evaluateExpression(expr) { actual =>
-          actual should be(expected)
+          expected match {
+            case SimpleValue(d: Double) =>
+              actual match {
+                case SimpleValue(ad: Double) =>
+                  // Required for NaN comparison
+                  java.lang.Double.compare(d, ad) should be (0)
+                case other => fail(s"Expected $other to be a SimpleValue(Double)")
+              }
+            case _ =>
+              actual should be(expected)
+          }
         }
       }
     }
