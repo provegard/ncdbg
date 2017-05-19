8a01273a92bc83301155f8f7c27ecf10ec11469b
Per Rovegård <per@rovegard.se>
2017-05-19 17:11:14 +0200

    Restructuring the step tests

--- src/test/scala/com/programmaticallyspeaking/ncd/nashorn/StepTest.scala
+++ src/test/scala/com/programmaticallyspeaking/ncd/nashorn/StepTest.scala
@@ -5,100 +5,106 @@ import com.programmaticallyspeaking.ncd.host.{StepInto, StepOut, StepOver}
 class StepTest extends StepTestFixture {
 
   "Stepping" - {
-    "works for regular step over" in {
-      val script =
-        """var x = 5;
-          |debugger;
-          |x = x + 1;
-        """.stripMargin
-      stepInScript(script, Seq(StepOver)) { bp =>
-        bp.location.lineNumber1Based should be (3)
+    "over" - {
+      "passes a debugger statement" in {
+        val script =
+          """var x = 5;
+            |debugger;
+            |x = x + 1;
+          """.stripMargin
+        stepInScript(script, Seq(StepOver)) { bp =>
+          bp.location.lineNumber1Based should be(3)
+        }
+      }
+
+      "steps out of a function" in {
+        val script =
+          """var fun = function() {
+            |  debugger;
+            |};
+            |fun();
+            |fun.toString();
+          """.stripMargin
+        stepInScript(script, Seq(StepOver)) { bp =>
+          bp.location.lineNumber1Based should be (5)
+        }
+      }
+
+      "steps out of two functions" in {
+        val script =
+          """var foo = function() {
+            |  debugger;
+            |  return 42;
+            |};
+            |var bar = function() {
+            |  return foo();
+            |};
+            |bar();
+            |bar.toString();
+          """.stripMargin
+        stepInScript(script, Seq(StepOver, StepOver)) { bp =>
+          bp.location.lineNumber1Based should be (9)
+        }
+      }
+
+      "passes a function call" in {
+        val script =
+          """var fun = function() {
+            |  return 'testing';
+            |};
+            |debugger;
+            |fun();
+            |fun.toString();
+          """.stripMargin
+        stepInScript(script, Seq(StepOver, StepOver)) { bp =>
+          bp.location.lineNumber1Based should be (6)
+        }
       }
     }
 
-    "steps out of a function" in {
-      val script =
-        """var fun = function() {
-          |  debugger;
-          |};
-          |fun();
-          |fun.toString();
-        """.stripMargin
-      stepInScript(script, Seq(StepOut)) { bp =>
-        bp.location.lineNumber1Based should be (5)
+    "out" - {
+      "exits a function" in {
+        val script =
+          """var fun = function() {
+            |  debugger;
+            |};
+            |fun();
+            |fun.toString();
+          """.stripMargin
+        stepInScript(script, Seq(StepOut)) { bp =>
+          bp.location.lineNumber1Based should be (5)
+        }
       }
     }
 
-    "steps into a function" in {
-      val script =
-        """var fun = function() {
-          |  return 42;
-          |};
-          |fun(); // force compilation of 'fun'
-          |debugger;
-          |fun();
-        """.stripMargin
-      stepInScript(script, Seq(StepOver, StepInto)) { bp =>
-        bp.location.lineNumber1Based should be (2)
+    "into" - {
+      "enters a function" in {
+        val script =
+          """var fun = function() {
+            |  return 42;
+            |};
+            |fun(); // force compilation of 'fun'
+            |debugger;
+            |fun();
+          """.stripMargin
+        stepInScript(script, Seq(StepOver, StepInto)) { bp =>
+          bp.location.lineNumber1Based should be (2)
+        }
       }
-    }
 
-    "steps into a function after step over" in {
-      val script =
-        """var fun = function() {
-          |  return 42;
-          |};
-          |fun(); // force compilation of 'fun'
-          |debugger;
-          |var f = fun;
-          |f();
-        """.stripMargin
-      stepInScript(script, Seq(StepOver, StepOver, StepInto)) { bp =>
-        bp.location.lineNumber1Based should be (2)
-      }
-    }
-
-    "steps out of a function via step-over" in {
-      val script =
-        """var fun = function() {
-          |  debugger;
-          |};
-          |fun();
-          |fun.toString();
-        """.stripMargin
-      stepInScript(script, Seq(StepOver)) { bp =>
-        bp.location.lineNumber1Based should be (5)
-      }
-    }
-
-    "steps out of two functions via step-over" in {
-      val script =
-        """var foo = function() {
-          |  debugger;
-          |  return 42;
-          |};
-          |var bar = function() {
-          |  return foo();
-          |};
-          |bar();
-          |bar.toString();
-        """.stripMargin
-      stepInScript(script, Seq(StepOver, StepOver)) { bp =>
-        bp.location.lineNumber1Based should be (9)
-      }
-    }
-
-    "steps over a function call" in {
-      val script =
-        """var fun = function() {
-          |  return 'testing';
-          |};
-          |debugger;
-          |fun();
-          |fun.toString();
-        """.stripMargin
-      stepInScript(script, Seq(StepOver, StepOver)) { bp =>
-        bp.location.lineNumber1Based should be (6)
+      "enters a function after step over" in {
+        val script =
+          """var fun = function() {
+            |  return 42;
+            |};
+            |fun(); // force compilation of 'fun'
+            |debugger;
+            |var f = fun;
+            |f();
+          """.stripMargin
+        stepInScript(script, Seq(StepOver, StepOver, StepInto)) { bp =>
+          bp.location.lineNumber1Based should be (2)
+        }
       }
     }
   }
