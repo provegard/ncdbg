041fc462d9bb9a4e060fa22befe18a40a48fc0db
Per Rovegård <per@rovegard.se>
2017-08-11 23:36:42 +0200

    Java 9 tests: Use JAVA9_HOME env variable

--- src/test/scala/com/programmaticallyspeaking/ncd/nashorn/java9/RunningJava9.scala
+++ src/test/scala/com/programmaticallyspeaking/ncd/nashorn/java9/RunningJava9.scala
@@ -2,6 +2,10 @@ package com.programmaticallyspeaking.ncd.nashorn.java9
 
 import com.programmaticallyspeaking.ncd.nashorn.NashornScriptHostTestFixture
 
+object RunningJava9 {
+  val java9Home = Option(System.getenv("JAVA9_HOME")).getOrElse("c:\\Program files\\Java\\jdk-9")
+}
+
 trait RunningJava9 { self: NashornScriptHostTestFixture =>
-  protected override def javaHome: Option[String] = Some("c:\\Program files\\Java\\jdk-9")
+  protected override def javaHome: Option[String] = Some(RunningJava9.java9Home)
 }
