87a64415560b9190b22037a711e7ace602fff1d6
Per Rovegård <per@rovegard.se>
2017-09-14 22:08:11 +0200

    Moving around classes and adding some ScalaDoc

--- src/main/scala/com/programmaticallyspeaking/ncd/host/ScriptEvent.scala
+++ src/main/scala/com/programmaticallyspeaking/ncd/host/ScriptEvent.scala
@@ -2,10 +2,36 @@ package com.programmaticallyspeaking.ncd.host
 
 trait ScriptEvent
 
+/**
+  * Emitted when the remote VM has paused, typically on a breakpoint. The breakpoint can be set by the user or be a
+  * 'debugger' statement.
+  *
+  * @param stackFrames the current script stack frames
+  * @param breakpointId the ID of the breakpoint; for a 'debugger' statement, the ID is not stable
+  */
 case class HitBreakpoint(stackFrames: Seq[StackFrame], breakpointId: String) extends ScriptEvent
 
+/**
+  * Emitted when the remote VM resumes execution.
+  */
 case object Resumed extends ScriptEvent
 
+/**
+  * Emitted when a script has been discovered.
+  *
+  * @param script the script
+  */
 case class ScriptAdded(script: Script) extends ScriptEvent
 
+/**
+  * Emitted when an uncaught error is detected.
+  *
+  * @param error the error
+  */
 case class UncaughtError(error: ErrorValue) extends ScriptEvent
+
+/**
+  * Emitted when the host considers initialization to be complete; currently when all needed reference types have been
+  * identified.
+  */
+object InitialInitializationComplete extends ScriptEvent
--- src/main/scala/com/programmaticallyspeaking/ncd/nashorn/NashornDebuggerHost.scala
+++ src/main/scala/com/programmaticallyspeaking/ncd/nashorn/NashornDebuggerHost.scala
@@ -1267,30 +1267,4 @@ class NashornDebuggerHost(val virtualMachine: VirtualMachine, protected val asyn
   }
 }
 
-object InitialInitializationComplete extends ScriptEvent
-
-/**
-  * An active breakpoint may map to one or more breakable locations, since we cannot distinguish between location
-  * column numbers.
-  *
-  * @param id the breakpoint ID
-  * @param breakableLocations the breakable locations
-  */
-case class ActiveBreakpoint(id: String, breakableLocations: Seq[BreakableLocation], condition: Option[String]) {
-  assert(breakableLocations.nonEmpty, "An active breakpoint needs at least one breakable location")
-
-  val firstBreakableLocation = breakableLocations.head
-
-  def toBreakpoint: Breakpoint = {
-    val script = firstBreakableLocation.script
-    // There may be multiple breakable locations for a line (each with its own Location), but to DevTools we
-    // only report unique locations.
-    Breakpoint(id, script.id, Some(script.url), breakableLocations.map(_.scriptLocation).distinct)
-  }
-
-  def disable(): Unit = breakableLocations.foreach(_.disable())
-  def enable(): Unit = breakableLocations.foreach(_.enable())
-
-  def contains(breakableLocation: BreakableLocation) = breakableLocations.contains(breakableLocation)
-}
 
