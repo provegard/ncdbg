71903d9bf58841d87de846b506c582aabb405809
Per Rovegård <per@rovegard.se>
2017-09-26 23:20:27 +0200

    Adapt the RunningJava9 trait to Java 9, i.e. don't switch JVM if the current one is 9...

--- src/test/scala/com/programmaticallyspeaking/ncd/nashorn/java9/RunningJava9.scala
+++ src/test/scala/com/programmaticallyspeaking/ncd/nashorn/java9/RunningJava9.scala
@@ -3,13 +3,21 @@ package com.programmaticallyspeaking.ncd.nashorn.java9
 import com.programmaticallyspeaking.ncd.nashorn.NashornScriptHostTestFixture
 
 import scala.concurrent.duration._
+import scala.util.Try
 
 object RunningJava9 {
-  val java9Home = Option(System.getenv("JAVA9_HOME")).getOrElse("c:\\Program files\\Java\\jdk-9")
+  def java9Home: Option[String] = {
+    if (isJava9) None
+    else Option(System.getenv("JAVA9_HOME")).orElse(Some("c:\\Program files\\Java\\jdk-9"))
+  }
+
+  private lazy val isJava9 = {
+    Try(Class.forName("jdk.nashorn.internal.runtime.Symbol")).isSuccess
+  }
 }
 
 trait RunningJava9 { self: NashornScriptHostTestFixture =>
   // Java 9 on Travis is slow to start, increase the timeout a bit
   override val runVMTimeout: FiniteDuration = 16.seconds
-  protected override def javaHome: Option[String] = Some(RunningJava9.java9Home)
+  protected override def javaHome: Option[String] = RunningJava9.java9Home
 }
