795a7ce042cf276ac9631db979431c3835ba8af6
Per Rovegård <per@rovegard.se>
2018-07-28 01:30:43 +0200

    Handle SourceMap 'sourceRoot' attribute
    Resolve sources relative to the source root.

--- src/main/scala/com/programmaticallyspeaking/ncd/infra/SourceMap.scala
+++ src/main/scala/com/programmaticallyspeaking/ncd/infra/SourceMap.scala
@@ -1,13 +1,18 @@
 package com.programmaticallyspeaking.ncd.infra
 
-private case class SourceMapData(sources: Seq[String])
+private case class SourceMapData(sources: Seq[String], sourceRoot: Option[String])
 
 class SourceMap(json: String) {
   private val data = ObjectMapping.fromJson[SourceMapData](json)
 
   // Transcrypt (Python to JS transpiler) includes an empty entry in the sources section of the source map.
   // Remove it, as it breaks URL resolution.
-  def sources: Seq[String] = data.sources.filter(_ != "")
+  // Note: We assume sourceRoot ends with the appropriate slash here.
+  def sources: Seq[String] = data.sources.map(resolveAgainstSourceRoot).filter(_ != "")
+
+  private def resolveAgainstSourceRoot(path: String): String = {
+    data.sourceRoot.map(_ + path).getOrElse(path)
+  }
 }
 
 object SourceMap {
